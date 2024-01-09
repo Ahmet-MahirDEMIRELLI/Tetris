660f60a120fe03b70792de7c5247c8e35ba22ec8
Ahmet Mahir Demirelli <122171884+Ahmet-MahirDEMIRELLI@users.noreply.github.com>
2024-01-09 16:43:47 +0300

    Update Tetris.c

--- 21011063.c
+++ 21011063.c
@@ -1,6 +1,7 @@
 /*
-   @brief Bu proje 2023-2024 Güz Dönemi, Bilgisayar Bilimlerine Giriş dersi için yapılmıştır.
-   @brief This project is done for 2023-2024 Fall Semester, Introduction to Computer Seciences lecture.
+   @brief YTU
+   @brief Bu proje Ahmet Mahir Demirelli(21011063) tarafından, 2023-2024 Güz Dönemi, Bilgisayar Bilimlerine Giriş dersi için yapılmıştır.
+   @brief This project is done by Ahmet Mahir Demirelli(21011063) for 2023-2024 Fall Semester, Introduction to Computer Seciences lecture.
 */
 #include<stdio.h>
 #include<time.h>
@@ -11,6 +12,7 @@
 #define PIECE_ROW 3										
 #define PIECE_COLUMN 3
 void clean(int[]);
+int language;
 int main(){
 	bool tetris(int []);
 	bool isDone;
@@ -20,7 +22,12 @@ int main(){
 	do{
 		isDone = tetris(score);	
 	}while(!isDone);
-	printf("\n\n	Elde edilen max skor : %d",score[1]);
+	if(language == 1){
+		printf("\n\n	Elde edilen max skor : %d",score[1]);
+	}
+	else{
+		printf("\n\n	Max score : %d",score[1]);
+	}
 	return 0;
 }
 bool tetris(int score[]){
@@ -45,12 +52,22 @@ bool tetris(int score[]){
 	do{
 		isDone = makeMove(matrix,size,score);
 	}while(!isDone);
-	printf("\n\n	Skorunuz : %d",score[0]);
+	if(language == 1){
+		printf("\n\n	Skorunuz : %d",score[0]);
+	}
+	else{
+		printf("\n\n	Score : %d",score[0]);
+	}
 	if(score[0] > score[1]){
 		score[1] = score[0];
 	}
 	score[0] = 0;
-	printf("	Devam etmek icin enter'a basiniz		");
+	if(language == 1){
+		printf("	Devam etmek icin enter'a basiniz		");
+	}
+	else{
+		printf("	Press enter to continue		");
+	}
 	getch();
 	return false;
 }
@@ -87,8 +104,14 @@ bool makeMove(int matrix[][MAX_SIZE],int size, int score[]){
 	}
 	fillPieceMatrix(piece,pieceNo);						
 	if(!playable){
-		printf("	Parca yerlestirilemez. Oyun bitti.\n");
-		printf("	Devam etmek icin enter'a basiniz		");
+		if(language == 1){
+			printf("	Parca yerlestirilemez. Oyun bitti.\n");
+			printf("	Devam etmek icin enter'a basiniz		");
+		}
+		else{
+			printf("	The piece cannot be placed. Game over.\n");
+			printf("	Press enter to continue		");
+		}
 		getch();
 		return true;
 	}
@@ -99,8 +122,14 @@ bool makeMove(int matrix[][MAX_SIZE],int size, int score[]){
 		printf("\n\n");
 		printPiece(piece,PIECE_ROW);
 		if(pieceNo != 2 && pieceNo != 6){
-			printf("	Parcayi saat yonune 90 derece dondurmek icin 1 giriniz			");
-			scanf("%d",&choise);
+			if(language == 1){
+				printf("	Parcayi saat yonune 90 derece dondurmek icin 1 giriniz			");
+				scanf("%d",&choise);
+			}
+			else{
+				printf("	Enter 1 to rotate the piece 90 degrees clockwise.			");
+				scanf("%d",&choise);
+			}
 			if(choise == 0){
 				clean(score);
 				printGameRules();
@@ -108,8 +137,14 @@ bool makeMove(int matrix[][MAX_SIZE],int size, int score[]){
 				printGameTable(matrix,size);
 				printf("\n\n");
 				printPiece(piece,PIECE_ROW);
-				printf("	Parcayi saat yonune 90 derece dondurmek icin 1 giriniz			");
-				scanf("%d",&choise);
+				if(language == 1){
+					printf("	Parcayi saat yonune 90 derece dondurmek icin 1 giriniz			");
+					scanf("%d",&choise);
+				}
+				else{
+					printf("	Enter 1 to rotate the piece 90 degrees clockwise.			");
+					scanf("%d",&choise);
+				}
 			}
 			else if(choise == -1){
 				score[0] = 0;
@@ -121,8 +156,14 @@ bool makeMove(int matrix[][MAX_SIZE],int size, int score[]){
 				printGameTable(matrix,size);
 				printf("\n\n");
 				printPiece(piece,PIECE_ROW);
-				printf("	Parcayi saat yonune 90 derece dondurmek icin 1 giriniz			");
-				scanf("%d",&choise);
+				if(language == 1){
+					printf("	Parcayi saat yonune 90 derece dondurmek icin 1 giriniz			");
+					scanf("%d",&choise);
+				}
+				else{
+					printf("	Enter 1 to rotate the piece 90 degrees clockwise.			");
+					scanf("%d",&choise);
+				}
 				if(choise == 0){
 					clean(score);
 					printGameRules();
@@ -130,8 +171,14 @@ bool makeMove(int matrix[][MAX_SIZE],int size, int score[]){
 					printGameTable(matrix,size);
 					printf("\n\n");
 					printPiece(piece,PIECE_ROW);
-					printf("	Parcayi saat yonune 90 derece dondurmek icin 1 giriniz			");
-					scanf("%d",&choise);
+					if(language == 1){
+						printf("	Parcayi saat yonune 90 derece dondurmek icin 1 giriniz			");
+						scanf("%d",&choise);
+					}
+					else{
+						printf("	Enter 1 to rotate the piece 90 degrees clockwise.			");
+						scanf("%d",&choise);
+					}
 				}
 				else if(choise == -1){
 					score[0] = 0;
@@ -139,8 +186,14 @@ bool makeMove(int matrix[][MAX_SIZE],int size, int score[]){
 				}
 			}
 		}
-		printf("	Parca hangi sutuna yerlestirilsin		");
-		scanf("%d",&j);
+		if(language == 1){
+			printf("	Parca hangi sutuna yerlestirilsin		");
+			scanf("%d",&j);
+		}
+		else{
+			printf("	In which column would you like to place the piece		");
+			scanf("%d",&j);
+		}
 		if(j == 0){
 			clean(score);
 			printGameRules();
@@ -148,18 +201,29 @@ bool makeMove(int matrix[][MAX_SIZE],int size, int score[]){
 			printGameTable(matrix,size);
 			printf("\n\n");
 			printPiece(piece,PIECE_ROW);
-			printf("	Parca hangi sutuna yerlestirilsin		");
-			scanf("%d",&j);
+			if(language == 1){
+				printf("	Parca hangi sutuna yerlestirilsin		");
+				scanf("%d",&j);
+			}
+			else{
+				printf("	In which column would you like to place the piece		");
+				scanf("%d",&j);
+			}
 		}
 		else if(j == -1){
 			score[0] = 0;
 			return true;
 		}
-		j--;                                                      // kullanýcý için 1 matris için 0
+		j--;                                                      // kullanıcı için 1 matris için 0
 		i = tryInsertion(matrix,size,piece,j);
 		clean(score);      												                       		  
 		if(i < 0){
-			printf("	Parca %d. sutuna yerlestirilemez. Lutfen tekrar giris yapiniz\n",j + 1);
+			if(language == 1){
+				printf("	Parca %d. sutuna yerlestirilemez. Lutfen tekrar deneyiniz\n",j + 1);
+			}
+			else{
+				printf("	The piece cannot be placed in the %d. column. Please try again\n",j + 1);
+			}
 			getch();
 		}
 		else{
@@ -209,8 +273,8 @@ int tryInsertion(int matrix[][MAX_SIZE], int size, int piece[][PIECE_COLUMN],int
 			}
 		}
 	}
-	if(column_of_first == 0){                   // sol üst piece[0][0] da : matrix üzerinden column, column + 1 ve column + 2 ile ilgilenicem
-		if(num_of_row == 1){					// parça 1 satýr
+	if(column_of_first == 0){                   
+		if(num_of_row == 1){					
 			i = size - 1;
 			possible = false;
 			while(!possible && i >= 0){
@@ -234,7 +298,7 @@ int tryInsertion(int matrix[][MAX_SIZE], int size, int piece[][PIECE_COLUMN],int
 				i--;
 			}
 		}
-		else if(num_of_row == 2){				// parça 2 satýr
+		else if(num_of_row == 2){				
 			i = size - 2;
 			possible = false;
 			while(!possible && i >= 0){
@@ -266,7 +330,7 @@ int tryInsertion(int matrix[][MAX_SIZE], int size, int piece[][PIECE_COLUMN],int
 				i--;	
 			}
 		}
-		else{								   // parça 3 satýr
+		else{								   
 			i = size - 3;
 			possible = false;
 			while(!possible && i >= 0){
@@ -299,8 +363,7 @@ int tryInsertion(int matrix[][MAX_SIZE], int size, int piece[][PIECE_COLUMN],int
 			}								
 		}
 	}
-	else if(column_of_first == 1){			   // sol üst piece[0][1] de : matrix üzerinden column - 1, column ve column + 1 ile ilgilenicem
-		                                       // S parçasý;                                       
+	else if(column_of_first == 1){			                                         
 		i = size - 2;
 		possible = false;
 		while(!possible && i >= 0){
@@ -333,8 +396,7 @@ int tryInsertion(int matrix[][MAX_SIZE], int size, int piece[][PIECE_COLUMN],int
 			i--;	
 		}
 	}
-	else{							     	   // sol üst piece[0][2] de : matrix üzerinden column - 2, column -1  ve column  ile ilgilenicem
-											   // 3 kere döndürülmüþ L parçasý
+	else{							     	   
 		i= size - 2;
 		possible = false;
 		while(!possible && i >= 0){
@@ -386,8 +448,8 @@ void updateMatrix(int matrix[][MAX_SIZE], int size, int piece[][PIECE_COLUMN],in
 			}
 		}
 	}
-	if(column_of_first == 0){                   // sol üst piece[0][0] da : matrix üzerinden column, column + 1 ve column + 2 ile ilgilenicem
-		if(num_of_row == 1){					// parça 1 satýr
+	if(column_of_first == 0){                  
+		if(num_of_row == 1){					
 			column_of_piece = 0;
 			i = row;
 			j = column;
@@ -399,7 +461,7 @@ void updateMatrix(int matrix[][MAX_SIZE], int size, int piece[][PIECE_COLUMN],in
 				column_of_piece++;
 			}
 		}
-		else if(num_of_row == 2){				// parça 2 satýr
+		else if(num_of_row == 2){				
 				i = row;
 				row_of_piece = 0;
 				while(i <= row + 1){
@@ -416,7 +478,7 @@ void updateMatrix(int matrix[][MAX_SIZE], int size, int piece[][PIECE_COLUMN],in
 					i++;
 				}	
 		}
-		else{								   // parça 3 satýr
+		else{								   
 				i = row;
 				row_of_piece = 0;
 				while(i <= row + 2){
@@ -434,8 +496,7 @@ void updateMatrix(int matrix[][MAX_SIZE], int size, int piece[][PIECE_COLUMN],in
 				}								
 		}
 	}
-	else if(column_of_first == 1){			   // sol üst piece[0][1] de : matrix üzerinden column - 1, column ve column + 1 ile ilgilenicem
-		                                       // S parçasý
+	else if(column_of_first == 1){			   
 		i = row;
 		row_of_piece = 0;
 		while(i <= row + 1){
@@ -452,8 +513,7 @@ void updateMatrix(int matrix[][MAX_SIZE], int size, int piece[][PIECE_COLUMN],in
 			i++;
 		}	
 	}
-	else{							     	   // sol üst piece[0][2] de : matrix üzerinden column - 2, column -1  ve column  ile ilgilenicem
-											   // 3 kere döndürülmüþ L parçasý
+	else{							     	   
 		i = row;
 		row_of_piece = 0;
 		while(i <= row + 1){
@@ -700,20 +760,50 @@ int MENU(int score[]){
 	void clean(int []);
 	int choise,size;
 	clean(score);
-	printf("	Yeni oyun acmak icin icin 1\n	Programi sonlandirmak icin 0 giriniz			");
-	scanf("%d",&choise);
-	while(choise != 0 && choise != 1){
-		printf("	Hatali giris yaptiniz lutfen tekrar deneyin\n");
-		printf("	Yeni oyun acmak icin icin 1\n	Programi sonlandirmak icin 0 giriniz		");
+	do{
+		printf("	Turkce icin 1 giriniz\n	Enter 2 for English		");
+		scanf("%d",&language);
+		clean(score);
+	}while(language != 1 && language != 2);
+	if(language == 1){
+		printf("	Yeni oyun acmak icin icin 1\n	Programi sonlandirmak icin 0 giriniz			");
 		scanf("%d",&choise);
+	}
+	else{
+		printf("	Press 1 for new game\n	Press 0 to quit			");
+		scanf("%d",&choise);
+	}
+	while(choise != 0 && choise != 1){
+		if(language == 1){
+			printf("	Hatali giris yaptiniz lutfen tekrar deneyin\n");
+			printf("	Yeni oyun acmak icin icin 1\n	Programi sonlandirmak icin 0 giriniz		");
+			scanf("%d",&choise);
+		}
+		else{
+			printf("	Incorrect input, please try again\n");
+			printf("	Press  1 for new game\n	Press 0 to quit			");
+			scanf("%d",&choise);
+		}
 	}
 	if(choise == 1){
 		clean(score);
-		printf("	Oyun tahtasi boyutunu giriniz		");
-		scanf("%d",&size);
-		while(size < MIN_SIZE || size > MAX_SIZE){
-			printf("	Oyun alani en az %dx%d en fazla %dx%d olabilir. Bu durumu goz onune alarak tekrar deger giriniz		",MIN_SIZE,MIN_SIZE,MAX_SIZE,MAX_SIZE);
+		if(language == 1){
+			printf("	Oyun tahtasi boyutunu giriniz (NxN oyun alani icin N giriniz)		");
 			scanf("%d",&size);
+		}
+		else{
+			printf("	Enter the size of game table (Enter N for NxN game table)		");
+			scanf("%d",&size);
+		}
+		while(size < MIN_SIZE || size > MAX_SIZE){
+			if(language == 1){
+				printf("	Oyun alani en az %dx%d en fazla %dx%d olabilir. Bu durumu goz onune alarak tekrar deger giriniz		",MIN_SIZE,MIN_SIZE,MAX_SIZE,MAX_SIZE);
+				scanf("%d",&size);
+			}
+			else{
+				printf("	Min size of the game table is : %dx%d, Max size of the game table is %dx%d. Please try again		",MIN_SIZE,MIN_SIZE,MAX_SIZE,MAX_SIZE);
+				scanf("%d",&size);
+			}
 		}
 	}
 	else{
@@ -722,17 +812,32 @@ int MENU(int score[]){
 	return size;
 }
 void printGameRules(){
-	printf("	Oyun kurallari: \n");
-	printf("		1: Size her adimda parca ve oyun alani sunulacaktir ve sizden sutun numarasi istenecektir\n");
-	printf(" Ornek: 7 girmeniz durumunda 7. sutundaki uygun olan en alttaki karaye parcanin en ust satirinda bulunan en soldaki kare gelecek sekilde parca oyun alanina yerlestirilecektir.\n");
-	printf(" (Not: Eger parca o alana yerlestirilemiyorsa sizden tekrar bilgi istenir)\n");
-	printf("		2: Oyun alanindaki x ifadeleri bos alani 1 ifadeleri ise dolu alani ifade eder.\n");
-	printf("		3: Oyun alaninin en ust satirina parcanin bir kismi gelecek sekilde ekleme yapildiginda en az bir satir parcalanmiyorsa oyun biter\n");
-	printf("		4: Sutun numaralari oyun alaninin ustunde bulunur.(Not:Hizzayi kaydirmamasi icin 9 dan sonra 0 ile devam eder yani 10 u ifade eder)\n");
-	printf("		5: Oyunun tamamlanmasi sonucunda skorunuz maksimum skor ise kayit edilir\n");
-	printf("		6: Sizden bilgi istenen herhangi bir durumda -1 girerek oyunu sonlandirabilirsiniz. (Not : anlik skorunuz gecersiz sayilir)\n");
-	printf("		7: Sizden bilgi istenen herhangi bir durumda 0 girerek oyun kurallarini goruntuleyebilirsiniz. (Not: arka arkaya kullanilamaz)\n (Not:0 kullanildiktan hemen sonra -1 kullanilamaz)\n");
-	printf("	Devam etmek icin enter'a basiniz		");
+	if(language == 1){
+		printf("	Oyun kurallari: \n");
+		printf("		1: Size her adimda parca ve oyun alani sunulacaktir ve sizden sutun numarasi istenecektir\n");
+		printf(" Ornek: 7 girmeniz durumunda 7. sutundaki uygun olan en alttaki karaye parcanin en ust satirinda bulunan en soldaki kare gelecek sekilde parca oyun alanina yerlestirilecektir.\n");
+		printf(" (Not: Eger parca o alana yerlestirilemiyorsa sizden tekrar bilgi istenir)\n");
+		printf("		2: Oyun alanindaki x ifadeleri bos alani 1 ifadeleri ise dolu alani ifade eder.\n");
+		printf("		3: Oyun alaninin en ust satirina parcanin bir kismi gelecek sekilde ekleme yapildiginda en az bir satir parcalanmiyorsa oyun biter\n");
+		printf("		4: Sutun numaralari oyun alaninin ustunde bulunur.(Not:Hizzayi kaydirmamasi icin 9 dan sonra 0 ile devam eder yani 10 u ifade eder)\n");
+		printf("		5: Oyunun tamamlanmasi sonucunda skorunuz maksimum skor ise kayit edilir\n");
+		printf("		6: Sizden bilgi istenen herhangi bir durumda -1 girerek oyunu sonlandirabilirsiniz. (Not : anlik skorunuz gecersiz sayilir)\n");
+		printf("		7: Sizden bilgi istenen herhangi bir durumda 0 girerek oyun kurallarini goruntuleyebilirsiniz. (Not: arka arkaya kullanilamaz)\n (Not:0 kullanildiktan hemen sonra -1 kullanilamaz)\n");
+		printf("	Devam etmek icin enter'a basiniz		");
+	}
+	else{
+		printf("	Game rules: \n");
+		printf("		1: At each step you will be presented with the piece and playing field and you will be asked for the column number.\n");
+		printf(" Examle: If you enter 7, the piece will be placed on the playing field with the leftmost square in the top row of the piece being the lowest square in the \n 7th column.\n");
+		printf(" (Note: If the piece cannot be placed in that column, you will be asked for input again.)\n");
+		printf("		2: The x expressions on the playing field indicate the empty area, and the expressions 1 represent the filled area.\n");
+		printf("		3: When a part of the piece is added to the top line of the playing field, if at least one line is not broken, the game ends.\n");
+		printf("		4: Column numbers are located on the playing field. (Note: In order not to shift the alignment, it continues with 0 after 9, which means 10)\n");
+		printf("		5: Upon completion of the game, your score will be recorded as the maximum score if it tops the previous max score.\n");
+		printf("		6: You can end the game by entering -1 in any situation where you are asked for information. (Note: your instant score will be considered \n invalid)\n");
+		printf("		7: You can view the game rules by entering 0 whenever you are asked for information. (Note: cannot be used consecutively)\n (Note: -1 cannot be used immediately after 0 is used)\n");
+		printf("	Press enter to continue		");
+	}
 	getch();
 }
 void clean(int score[]){
